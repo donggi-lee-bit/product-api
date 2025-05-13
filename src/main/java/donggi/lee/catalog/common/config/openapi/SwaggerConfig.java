d00d2f31d317f94fd82d661756d9c6fa266b0bd9
donggi-lee-bit <devdonggilee@gmail.com>
2025-05-13 10:29:39 +0900

    feat: Swagger UI에서 토큰 사용 가능하게끔 설정

--- src/main/java/donggi/lee/catalog/common/config/openapi/SwaggerConfig.java
+++ src/main/java/donggi/lee/catalog/common/config/openapi/SwaggerConfig.java
@@ -1,16 +1,33 @@
 package donggi.lee.catalog.common.config.openapi;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI apiInfo() {
+    public OpenAPI openAPI() {
+        final var securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+        final var securityRequirement = new SecurityRequirement()
+            .addList("bearerAuth");
+
         return new OpenAPI()
+            .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .security(List.of(securityRequirement))
             .info(new Info()
                 .title("상품 관리 및 인증 API")
                 .version("v1")
